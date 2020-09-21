e3b2d02f1d7f304fa02a7cfefbdf7b29374762a9
Gabriel758Eduardo <eduardosantos9406@gmail.com>
2020-09-21 02:46:54 -0300

    Pacote de modificação relacionado a atualização dos dados do animal

---
+++ menu.php
@@ -0,0 +1,11 @@
+<?php
+	include("cabecalho.inc.php");
+?>
+
+		<a href="cad_animal.php">Cadastro do animal</a><br>
+		<a href="cons_todos_animais.php">Consultar todos os animais</a><br>
+		<a href="cons_animal.php">Consultar animal</a><br>
+		
+<?php
+	include("rodape.inc.php");
+?>
