262f3dc47ab3895afbb45a697d78f697a19da0b3
Gabriel758Eduardo <eduardosantos9406@gmail.com>
2020-09-21 02:48:25 -0300

    Nesse commit foi feito alterações na consulta dos animais acrescentando as operações de REMOVER/EDITAR.

---
+++ remove_animal.php
@@ -0,0 +1,11 @@
+<?php
+	$pront = $_GET['pront'];
+	
+	include('cabecalho_conexao.php');
+	
+	$SQL = "DELETE FROM animais WHERE id=$pront";
+
+	include('rodape_conexao.php');
+	
+	header('location:cons_todos_animais.php');
+?>
